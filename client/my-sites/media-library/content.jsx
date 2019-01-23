6cb585a8b0411a4b22f7c59b4322d65d695b42ad
John Godley <johng75@gmail.com>
2019-01-23 15:44:04 +0000

    Google Photos: update copy after migration
    Refer to Google Photos instead of Photos for Google

--- client/my-sites/media-library/content.jsx
+++ client/my-sites/media-library/content.jsx
@@ -232,7 +232,7 @@ class MediaLibraryContent extends React.Component {
 
 	renderGooglePhotosConnect() {
 		const connectMessage = this.props.translate(
-			'To show Photos from Google, you need to connect your Google account.'
+			'To show your Google Photos library you need to connect your Google account.'
 		);
 
 		return (
